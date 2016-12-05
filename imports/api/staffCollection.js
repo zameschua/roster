18fc3dd605adc282fcf8ac6597689faff4ffd61c
Zames <zames.chua@gmail.com>
2016-12-05 17:09:49 +0800

    Link meteor account to staffCollection on account creation

--- imports/api/staffCollection.js
+++ imports/api/staffCollection.js
@@ -1,13 +1,14 @@
 export const staffCollection = new Mongo.Collection("staffCollection");
 
 staffCollection.schema = new SimpleSchema({
-  name: {type: String, label: 'name',defaultValue: undefined},
-  preferredDates: {type: [Number], label: 'preferred dates',defaultValue: []},
+  meteorId: {type: String, label: 'Meteor Id'},
+  name: {type: String, label: 'name', defaultValue: "undefined"},
+  preferredDates: {type: [Number], label: 'preferred dates', defaultValue: []},
   blockOutDates: {type: [Number], label: 'block-out dates', defaultValue: []},
   carriedOverPoints: {type: Number, label: 'carried over points', defaultValue: 0},
   allocatedDates: {type: [Number], label: 'allocated dates', defaultValue: []},
   postOutDate: {type: Date, label: 'post out date', defaultValue: new Date()},
-  team: {type: String, label: 'team', defaultValue: undefined},
+  team: {type: String, label: 'team', defaultValue: "undefined"},
 });
 
 staffCollection.attachSchema(staffCollection.schema);
--- server/main.js
+++ server/main.js
@@ -12,6 +12,25 @@ Meteor.startup(() => {
 
 Meteor.methods({
 	deleteUser: function(id) {
+		//Meteor.users.remove(id); !Add this back in after setting up accounts properly
 		staffCollection.remove(id);
 	}
 });
+
+Accounts.onCreateUser(function(options, user) {
+	// Default hook's 'profile' behavior.
+	if (options.profile) {
+	    user.profile = options.profile;
+	}
+	console.log(options);
+	console.log(user);
+
+	// Add user to staffCollection
+	staffCollection.insert({
+		meteorId: user._id,
+	});
+
+	return user;
+});
+
+
